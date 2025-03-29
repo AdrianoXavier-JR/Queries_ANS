ac0930ff86b762095b50c60e2e418f136f7012cc
Adriano <adriano.xavier251311@gmail.com>
2025-03-28 23:11:51 -0300

    feat(ans): create api for to use in the front end

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.getcwd()))
-
+from flask import Flask, jsonify
 from download_files_ans.download_file import downloadFile
 from src.file_processor import find_top_values
 from src.db import fetch_nome_fantasia_by_reg_ans
 from datetime import datetime
 
-def main():
+app = Flask(__name__)
+
+@app.route('/top_values', methods=['GET'])
+def get_top_values():
+    # Baixar o arquivo e processar os dados
     downloadFile()
 
     last_year = datetime.now().year - 1
     quarter = 4
-
     path_csv = f"download_files_ans/ans_files/{last_year}/{quarter}T{last_year}.csv"
 
     top_values = find_top_values(path_csv, "VL_SALDO_FINAL")
 
     reg_ans_list = [item['REG_ANS'] for item in top_values]
-
     list = fetch_nome_fantasia_by_reg_ans("plano_saude_ativas", reg_ans_list)
 
-    print("As 10 operadoras com maiores despesas: ", list)
+    # Criar a resposta no formato JSON
+    response = {
+        "top_values": top_values,
+        "operadoras": list
+    }
+
+    return jsonify(response)
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
